8da5c6ff8aa880d8fa0134d2cf212d94e33e4a28
Xavier Watkins <xavier.watkins@gmail.com>
2018-10-05 15:36:35 +0100

    Config for dev and prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,9 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   context: __dirname,
-  devtool: 'inline-sourcemap',
   entry: ['@babel/polyfill', path.resolve(__dirname, 'src/ui/index.jsx')],
-  output: {
-    path: `${__dirname}/build`,
-    filename: 'app.[hash].bundle.js',
-  },
   resolve: {
     extensions: ['.jsx', '.js'],
   },
@@ -71,22 +64,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: `${__dirname}/public/index.html`,
+      template: `${__dirname}/index.html`,
       filename: 'index.html',
     }),
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3100,
-      proxy: 'http://localhost:39093/',
-      open: false,
-      ui: false,
-    }),
   ],
-  devServer: {
-    contentBase: path.join(__dirname, 'build'),
-    compress: true,
-    host: 'localhost',
-    port: 39093,
-    historyApiFallback: true,
-  },
 };
---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.[hash].bundle.js',
+  },
+  plugins: [
+    // Minify CSS
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+    }),
+  ],
+});
