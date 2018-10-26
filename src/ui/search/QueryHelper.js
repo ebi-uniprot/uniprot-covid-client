6cc575f88b25f7ccbc42ced3a55c609de5ea4491
Xavier Watkins <xavier.watkins@gmail.com>
2018-10-26 16:21:06 +0100

    Adding query helper and test cases for query string generation

---
+++ src/ui/search/QueryHelper.js
@@ -0,0 +1,50 @@
+// @flow
+import type { Field } from './AdvancedSearchField';
+
+const getItemTypePrefix = (itemType) => {
+  const itemTypeToPrefixMap = {
+    feature: 'ft_',
+    comment: 'cc_',
+  };
+  return itemTypeToPrefixMap[itemType] ? itemTypeToPrefixMap[itemType] : '';
+};
+
+const getItemTypeEvidencePrefix = (itemType) => {
+  const itemTypeToEvidencePrefixMap = {
+    feature: 'ftev_',
+    comment: 'ccev_',
+  };
+  return itemTypeToEvidencePrefixMap[itemType] ? itemTypeToEvidencePrefixMap[itemType] : '';
+};
+
+const getItemTypeRangePrefix = itemType => (itemType === 'feature' ? 'ftlen_' : '');
+
+const createSimpleSubquery = (field: Field) => `(${getItemTypePrefix(field.selectedNode.itemType)}${field.selectedNode.term}:${
+  field.queryInput.stringValue ? field.queryInput.stringValue : ''
+})`;
+
+const createRangeSubquery = (field: Field) => `(${getItemTypeRangePrefix(field.selectedNode.itemType)}${field.selectedNode.term}:[${
+  field.queryInput.rangeFrom ? field.queryInput.rangeFrom : ''
+} TO ${field.queryInput.rangeTo ? field.queryInput.rangeTo : ''}])`;
+
+const wrapIntoEvidenceSubquery = (field: Field, subQuery: string) => `(${subQuery}AND(${getItemTypeEvidencePrefix(field.selectedNode.itemType)}${
+  field.selectedNode.term
+}:${field.queryInput.evidenceValue}))`;
+
+const createQueryString = (queryFields: Array<Field>): string => queryFields.reduce((queryAccumulator: string, field: Field) => {
+  let query = '';
+  if (field.queryInput.stringValue && field.queryInput.stringValue !== '') {
+    query = `${query}${createSimpleSubquery(field)}`;
+  }
+  if (field.queryInput.rangeFrom || field.queryInput.rangeTo) {
+    query = `${query}${createRangeSubquery(field)}`;
+  }
+  if (field.queryInput.evidenceValue && field.queryInput.evidenceValue !== '') {
+    query = `${wrapIntoEvidenceSubquery(field, query)}`;
+  }
+  return `${queryAccumulator}${
+    queryAccumulator.length > 0 && query.length > 0 ? field.logic : ''
+  }${query}`;
+}, '');
+
+export default createQueryString;
