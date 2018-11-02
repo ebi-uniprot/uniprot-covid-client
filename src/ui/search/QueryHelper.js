9bd8e917c0dee204dee2a5715aa6d34d0e808fea
Xavier Watkins <xavier.watkins@gmail.com>
2018-11-02 15:58:32 +0000

    Adding go prefix

--- src/ui/search/QueryHelper.js
+++ src/ui/search/QueryHelper.js
@@ -5,6 +5,7 @@ const getItemTypePrefix = (itemType) => {
   const itemTypeToPrefixMap = {
     feature: 'ft_',
     comment: 'cc_',
+    goterm: 'go_',
   };
   return itemTypeToPrefixMap[itemType] ? itemTypeToPrefixMap[itemType] : '';
 };
