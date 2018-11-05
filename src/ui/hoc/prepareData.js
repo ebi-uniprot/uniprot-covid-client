a5e49bebc73dc948c747606d1d43327e7d53db71
Daniel Rice <Daniel.L.Rice@gmail.com>
2018-11-05 10:49:57 +0000

    Create a unique ID from API response items by joining properties: term-label-valuePrefix-itemType.

--- src/ui/hoc/prepareData.js
+++ src/ui/hoc/prepareData.js
@@ -1,11 +1,32 @@
 import { v1 as uuidv1 } from 'uuid';
 
-export default function appendUniqueValue(data) {
+function getUniqueIdFromItemProperties(item) {
+  let uid = '';
+  if (item.term) {
+    uid += `-${item.term}`;
+  }
+  if (item.label) {
+    uid += `-${item.label}`;
+  }
+  if (item.valuePrefix) {
+    uid += `-${item.valuePrefix}`;
+  }
+  if (item.itemType) {
+    uid += `-${item.itemType}`;
+  }
+  return uid;
+}
+
+export default function appendUniqueValue(data, idFromItemProperties = true) {
+  const idGenerator = idFromItemProperties ? getUniqueIdFromItemProperties : uuidv1;
   return data.map((item) => {
-    const newItem = { ...item, value: uuidv1() };
+    const newItem = {
+      ...item,
+      value: getUniqueIdFromItemProperties(item),
+    };
     const { items } = item;
     if (items) {
-      newItem.items = appendUniqueValue(items);
+      newItem.items = appendUniqueValue(items, idGenerator);
     }
     return newItem;
   });
