cb6407111466c6b370661bb11b54ca0c47ecf7f9
Daniel Rice <Daniel.L.Rice@gmail.com>
2018-11-11 21:48:08 +0000

    Action creators; move all action definitions to actions/index.js

---
+++ src/ui/actions/index.js
@@ -0,0 +1,36 @@
+export const SELECT_FIELD = 'SELECT_FIELD';
+export const CHANGE_INPUT_VALUE = 'CHANGE_INPUT_VALUE';
+export const CHANGE_EVIDENCE = 'CHANGE_EVIDENCE';
+export const CHANGE_RANGE_VALUE = 'CHANGE_RANGE_VALUE';
+export const CHANGE_LOGIC_OPERATOR = 'CHANGE_LOGIC_OPERATOR';
+export const HANDLE_FIELD_SELECT = 'HANDLE_FIELD_SELECT';
+export const SUBMIT_QUERY = 'SUBMIT_QUERY';
+export const ADD_CLAUSE = 'ADD_CLAUSE';
+
+export const selectField = (clauseId, fieldId, value) => ({
+
+});
+
+export const changeInputValue = (clauseId, fieldId, value) => ({
+
+});
+
+export const changeEvidence = (clauseId, fieldId, value) => ({
+
+});
+
+export const changeRangeValue = (clauseId, fieldId, value, from) => ({
+
+});
+
+export const changeLogicOperator = (clauseId, fieldId, value) => ({
+
+});
+
+export const submitQuery = () => ({
+
+});
+
+export const addClause = () => ({
+
+});
