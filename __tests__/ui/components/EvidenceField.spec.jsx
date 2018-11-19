8301cf90e3461a571d41f479fa230ffab4d65fb3
Daniel Rice <Daniel.L.Rice@gmail.com>
2018-11-19 15:28:44 +0000

    Add EvidenceField tests.

--- __tests__/ui/components/EvidenceField.spec.jsx
+++ __tests__/ui/components/EvidenceField.spec.jsx
@@ -1,37 +1,46 @@
 import React from 'react';
-import { shallow, mount, configure } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import renderer from 'react-test-renderer';
-import apiUrls from '../../../src/ui/apiUrls';
 import EvidenceField from '../../../src/ui/components/EvidenceField';
 
 configure({ adapter: new Adapter() });
 
+let wrapper;
+let props;
+
 describe('EvidenceField component', () => {
-  test('should render', () => {
-    const component = renderer
-      .create(<EvidenceField url={apiUrls.annotation_evidences} />)
-      .toJSON();
-    expect(component).toMatchSnapshot();
+  beforeEach(() => {
+    props = {
+      value: '',
+      handleChange: jest.fn(),
+      data: [
+        {
+          groupName: 'foo',
+          items: [
+            {
+              code: 0,
+              name: 'bar',
+            },
+            {
+              code: 1,
+              name: 'baz',
+            },
+          ],
+        },
+      ],
+    };
+
+    wrapper = shallow(<EvidenceField {...props} />);
   });
 
-  test('should fire change function on selection', () => {
-    const updateEvidence = jest.fn(() => {});
-    const wrapped = mount(
-      <EvidenceField url={apiUrls.annotation_evidences} updateEvidence={updateEvidence} />,
-    );
-    wrapped
+  test('should change evidence', () => {
+    wrapper
       .find('#evidence_select')
-      .first()
-      .simulate('change');
-    expect(updateEvidence).toHaveBeenCalled();
+      .simulate('change', { target: { value: props.data[0].items[0].code } });
+    expect(props.handleChange).toBeCalled();
   });
 
-  test('should set selectedEvidence in the props', () => {
-    const selectedEvidence = 'c1';
-    const wrapped = shallow(
-      <EvidenceField url={apiUrls.annotation_evidences} selectedEvidence={selectedEvidence} />,
-    );
-    expect(wrapped.instance().props.selectedEvidence).toBe(selectedEvidence);
+  test('should render', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 });
