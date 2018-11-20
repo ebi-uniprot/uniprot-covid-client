f3ca8ff7eed99ade622e82ae0b39f6b7dfa2761d
Xavier Watkins <xavier.watkins@gmail.com>
2018-11-20 14:03:04 +0000

    Allow to have text input AND range at the same time

--- src/ui/components/Field.jsx
+++ src/ui/components/Field.jsx
@@ -1,3 +1,4 @@
+import React, { Fragment } from 'react';
 import RangeField from './RangeField';
 import EnumField from './EnumField';
 import TextField from './TextField';
@@ -6,36 +7,41 @@ const Field = ({
   field, handleInputChange, handleRangeInputChange, queryInput,
 }) => {
   const { dataType, hasRange } = field;
-  if (dataType === 'enum') {
-    return EnumField({
-      field,
-      handleChange: handleInputChange,
-    });
+  switch (dataType) {
+    case 'enum':
+      return EnumField({
+        field,
+        handleChange: handleInputChange,
+      });
+    case 'date':
+      return RangeField({
+        field,
+        handleChange: handleRangeInputChange,
+      });
+    case 'string':
+    case 'integer':
+      return (
+        <Fragment>
+          <TextField
+            field={field}
+            handleChange={handleInputChange}
+            type={dataType}
+            value={queryInput.stringValue}
+          />
+          {hasRange && dataType !== 'integer' && (
+            <RangeField
+              field={field}
+              handleChange={handleRangeInputChange}
+              type="number"
+              rangeFrom={queryInput.rangeFrom}
+              rangeTo={queryInput.rangeTo}
+            />
+          )}
+        </Fragment>
+      );
+    default:
+      return null;
   }
-  if (dataType === 'date') {
-    return RangeField({
-      field,
-      handleChange: handleRangeInputChange,
-    });
-  }
-  if (dataType === 'string' || dataType === 'integer') {
-    return TextField({
-      field,
-      handleChange: handleInputChange,
-      type: dataType,
-      value: queryInput.stringValue,
-    });
-  }
-  if (hasRange && dataType !== 'integer') {
-    return RangeField({
-      field,
-      handleChange: handleRangeInputChange,
-      type: 'number',
-      rangeFrom: queryInput.rangeFrom,
-      rangeTo: queryInput.rangeTo,
-    });
-  }
-  return null;
 };
 
 export default Field;
