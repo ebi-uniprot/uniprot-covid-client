f27050687c26034b012de898c49a4abcd54a5357
Daniel Rice <Daniel.L.Rice@gmail.com>
2018-11-21 13:46:16 +0000

    Move suggester url creation to apiUrls.

--- src/ui/apiUrls.js
+++ src/ui/apiUrls.js
@@ -1,18 +1,24 @@
-const prefix = '//wwwdev.ebi.ac.uk/uniprot/api';
+import urljoin from 'url-join';
+
+const prefix = '//wwwdev.ebi.ac.uk/';
 
 export default {
   // uniprotkb advanced search terms
-  advanced_search_terms: `${prefix}/configure/uniprotkb/search_terms`,
+  advanced_search_terms: urljoin(prefix, '/uniprot/api/configure/uniprotkb/search_terms'),
   // Annotation evidence used by advanced search
-  annotation_evidences: `${prefix}/configure/uniprotkb/annotation_evidences`,
+  annotation_evidences: urljoin(prefix, '/uniprot/api/configure/uniprotkb/annotation_evidences'),
   // Go evidences used by advanced go search
   // "itemType": "goterm",
-  go_evidences: `${prefix}/configure/uniprotkb/go_evidences`,
+  go_evidences: urljoin(prefix, '/uniprot/api/configure/uniprotkb/go_evidences'),
   // Database cross references used by advanced search
-  database_xefs: `${prefix}/configure/uniprotkb/databases`,
+  database_xefs: urljoin(prefix, '/uniprot/api/configure/uniprotkb/databases'),
   // Database cross reference fields in result column configure
   // "itemType": "database",
-  database_fields: `${prefix}/configure/uniprotkb/databasefields`,
+  database_fields: urljoin(prefix, '/uniprot/api/configure/uniprotkb/databasefields'),
   // All result fields except database cross reference fields
-  results_fields: `${prefix}/configure/uniprotkb/resultfields`,
+  results_fields: urljoin(prefix, '/uniprot/api/configure/uniprotkb/resultfields'),
 };
+
+const RE_QUERY = /\?$/;
+
+export const getSuggesterUrl = (url, value) => urljoin(prefix, url.replace(RE_QUERY, value));
--- src/ui/search/AutocompleteWrapper.jsx
+++ src/ui/search/AutocompleteWrapper.jsx
@@ -1,12 +1,9 @@
 // @flow
 import React, { Component } from 'react';
-import urljoin from 'url-join';
 import { Autocomplete } from 'franklin-sites';
 import fetchData from '../hoc/fetchData';
 import appendUniqueId from '../hoc/prepareData';
-
-const SERVER = 'http://wwwdev.ebi.ac.uk/';
-const RE_QUERY = /\?$/;
+import { getSuggesterUrl } from '../apiUrls';
 
 type Props = {
   url: string,
@@ -27,7 +24,7 @@ type Chosen = {
   id: string,
   itemLabel: string,
   pathLabel: string,
-}
+};
 
 class AutocompleteWrapper extends Component<Props, State> {
   constructor(props: Props) {
@@ -37,9 +34,9 @@ class AutocompleteWrapper extends Component<Props, State> {
 
   handleChange = (textInputValue: string) => {
     const { url } = this.props;
-    const assembledUrl = urljoin(SERVER, url.replace(RE_QUERY, textInputValue));
-    this.fetchOptions(assembledUrl);
-  }
+    const suggesterUrl = getSuggesterUrl(url, textInputValue);
+    this.fetchOptions(suggesterUrl);
+  };
 
   fetchOptions = (url: string) => {
     fetchData(url)
@@ -49,12 +46,12 @@ class AutocompleteWrapper extends Component<Props, State> {
       .then(data => appendUniqueId(data, 'autocomplete'))
       .then(data => this.setState({ data }))
       .catch(e => console.error(e));
-  }
+  };
 
   handleSelect = (chosen: Chosen) => {
     const { onSelect } = this.props;
     onSelect(chosen.pathLabel);
-  }
+  };
 
   render() {
     const { data } = this.state;
