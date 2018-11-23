e3e7195f45e8d7696428df292ea2d9c633aa3ebc
Daniel Rice <Daniel.L.Rice@gmail.com>
2018-11-23 16:36:14 +0000

    Initial restructuring of files.

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import query from './reducers';
+import query from './advanced-search/state/reducers';
 import App from './App';
-import '../../node_modules/franklin-sites/dist/index.css';
-import '../styles/index.css';
-import initialState from './initialState';
+import '../node_modules/franklin-sites/dist/index.css';
+import './styles/index.css';
+import initialState from './advanced-search/initialState';
 
 const debug = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const store = createStore(query, initialState, debug(applyMiddleware(thunkMiddleware)));
--- src/ui/utils/index.js
+++
@@ -1,3 +0,0 @@
-export { default as createQueryString } from './query';
-export { default as fetchData } from './fetchData';
-export { default as createEmptyClause } from './clause';
