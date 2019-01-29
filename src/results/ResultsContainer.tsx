de778f60101edf699978f9710fab13f7a42a552d
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-01-29 15:38:45 +0000

    TypeScript fixes in ResultsContainer.

--- src/results/ResultsContainer.tsx
+++ src/results/ResultsContainer.tsx
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Dispatch, bindActionCreators } from 'redux';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { default as queryStringModule } from 'query-string';
 import { Facets } from 'franklin-sites';
-import {
-  fetchResults, addFacet, removeFacet, updateColumnSort,
-} from './state/actions';
-import { updateQueryString } from '../search/state/actions';
+import * as resultsActions from './state/actions';
+import * as searchActions from '../search/state/actions';
+import { Clause } from '../search/types/searchTypes';
 import SideBarLayout from '../layout/SideBarLayout';
 import ResultsTable from './ResultsTable';
+import { RootState, RootAction } from '../state/state-types';
 
-export class Results extends Component {
-  constructor(props) {
+interface IResultsProps extends RouteComponentProps {
+  queryString: string;
+  selectedFacets: [];
+  dispatchFetchResults: (
+    queryString: string,
+    columns: Array<string>,
+    selectedFacets: [],
+    sortBy: string,
+    sortDirection: string,
+  ) => void;
+  dispatchUpdateQueryString: (type: string) => void;
+  dispatchUpdateColumnSort: (column: string) => void;
+  dispatchAddFacet: (facetName: string, facetValue: string) => void;
+  dispatchRemoveFacet: (facetName: string, facetValue: string) => void;
+  clauses?: Array<Clause>;
+  columns: Array<string>;
+  sort: { column: string; direction: string };
+  results: [];
+  facets: [];
+  isFetching: boolean;
+}
+
+type ResultsContainerState = {
+  selectedRows: { [key: string]: boolean };
+};
+
+export class Results extends Component<IResultsProps, ResultsContainerState> {
+  constructor(props: IResultsProps) {
     super(props);
     this.state = { selectedRows: {} };
     this.handleRowSelect = this.handleRowSelect.bind(this);
@@ -26,11 +53,12 @@ export class Results extends Component {
       dispatchUpdateQueryString,
       columns,
       history,
+      sort: { column, direction },
     } = this.props;
-    dispatchFetchResults(queryString, columns, selectedFacets);
+    dispatchFetchResults(queryString, columns, selectedFacets, column, direction);
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps: IResultsProps) {
     const {
       location: { search: queryParamFromUrl },
       dispatchFetchResults,
@@ -52,7 +80,7 @@ export class Results extends Component {
     }
   }
 
-  handleRowSelect(rowId) {
+  handleRowSelect(rowId: string) {
     const { selectedRows: prevSelectedRows } = this.state;
     if (rowId in prevSelectedRows) {
       const { [rowId]: value, ...selectedRows } = prevSelectedRows;
@@ -104,7 +132,7 @@ export class Results extends Component {
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: RootState) => ({
   queryString: state.query.queryString,
   columns: state.results.columns,
   selectedFacets: state.results.selectedFacets,
@@ -114,13 +142,16 @@ const mapStateToProps = state => ({
   sort: state.results.sort,
 });
 
-const mapDispatchToProps = dispatch => ({
-  dispatchFetchResults: (query, columns, selectedFacets, sortBy, sortDirection) => dispatch(fetchResults(query, columns, selectedFacets, sortBy, sortDirection)),
-  dispatchAddFacet: (facetName, facetValue) => dispatch(addFacet(facetName, facetValue)),
-  dispatchRemoveFacet: (facetName, facetValue) => dispatch(removeFacet(facetName, facetValue)),
-  dispatchUpdateQueryString: queryString => dispatch(updateQueryString(queryString)),
-  dispatchUpdateColumnSort: column => dispatch(updateColumnSort(column)),
-});
+const mapDispatchToProps = (dispatch: Dispatch<RootAction>) => bindActionCreators(
+  {
+    dispatchFetchResults: (query, columns, selectedFacets, sortBy, sortDirection) => resultsActions.fetchResults(query, columns, selectedFacets, sortBy, sortDirection),
+    dispatchAddFacet: (facetName, facetValue) => resultsActions.addFacet(facetName, facetValue),
+    dispatchRemoveFacet: (facetName, facetValue) => resultsActions.removeFacet(facetName, facetValue),
+    dispatchUpdateQueryString: queryString => searchActions.updateQueryString(queryString),
+    dispatchUpdateColumnSort: column => resultsActions.updateColumnSort(column),
+  },
+  dispatch,
+);
 
 const ResultsContainer = withRouter(
   connect(
