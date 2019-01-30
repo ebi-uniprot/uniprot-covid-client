75e5b6a6a6f4d8bed5046328b015a0fb87cfccad
Xavier Watkins <xavier.watkins@gmail.com>
2019-01-30 13:24:14 +0000

    Take store out of index

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { FranklinSite } from 'franklin-sites';
-import { RootState } from './state/state-types';
 import HomePage from './pages/HomePage';
 import ResultsPage from './pages/ResultsPage';
 
 declare const BASE_URL: string;
 
-type AppProps = {
-  store: RootState;
-};
-
-const App: React.FC<AppProps> = ({ store }) => (
+const App = () => (
   <FranklinSite>
-    <Provider store={store}>
-      <Router basename={BASE_URL}>
-        <Switch>
-          <Route path="/" exact render={HomePage} />
-          <Route path="/uniprotkb" component={ResultsPage} />
-        </Switch>
-      </Router>
-    </Provider>
+    <Router basename={BASE_URL}>
+      <Switch>
+        <Route path="/" exact render={HomePage} />
+        <Route path="/uniprotkb" component={ResultsPage} />
+      </Switch>
+    </Router>
   </FranklinSite>
 );
 
