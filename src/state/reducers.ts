b2fd131971d6a04f3d0d92de27675e262bbdd214
Xavier Watkins <xavier.watkins@gmail.com>
2019-02-12 15:42:33 +0000

    Add root reducer to reset state when results component is unmounted

--- src/state/reducers.ts
+++ src/state/reducers.ts
@@ -1,10 +1,27 @@
-import {
-  combineReducers, Action, Reducer, ReducersMapObject,
-} from 'redux';
-import query from '../search/state/reducers';
-import results from '../results/state/reducers';
+import { combineReducers, Action, Reducer, ReducersMapObject } from 'redux';
+import query, { SearchAction } from '../search/state/reducers';
+import results, { ResultAction } from '../results/state/reducers';
+import { SearchState } from '../search/state/initialState';
+import { ResultsState } from '../results/state/initialState';
 
-export default combineReducers({
+type RootState =
+  | {
+      query: SearchState;
+      results: ResultsState;
+    }
+  | undefined;
+
+const appReducer = combineReducers({
   query,
   results,
 });
+
+const rootReducer = (state: RootState, action: SearchAction | ResultAction) => {
+  if (action.type === 'RESET') {
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+};
+
+export default rootReducer;
