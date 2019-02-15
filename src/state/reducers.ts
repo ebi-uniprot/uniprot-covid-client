8141dab2d59ccf0b88fed049e6ee2da03b8a6b6c
Xavier Watkins <xavier.watkins@gmail.com>
2019-02-15 10:22:27 +0000

    undefined for rootstate was in wrong place

--- src/state/reducers.ts
+++ src/state/reducers.ts
@@ -4,19 +4,20 @@ import results, { ResultAction } from '../results/state/reducers';
 import { SearchState } from '../search/state/initialState';
 import { ResultsState } from '../results/state/initialState';
 
-type RootState =
-  | {
-      query: SearchState;
-      results: ResultsState;
-    }
-  | undefined;
+type RootState = {
+  query: SearchState;
+  results: ResultsState;
+};
 
 const appReducer = combineReducers({
   query,
   results,
 });
 
-const rootReducer = (state: RootState, action: SearchAction | ResultAction) => {
+const rootReducer = (
+  state: RootState | undefined,
+  action: SearchAction | ResultAction
+) => {
   if (action.type === 'RESET') {
     state = undefined;
   }
