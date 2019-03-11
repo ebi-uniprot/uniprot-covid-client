58282eac34224fa3d5d44bbd0516bf9fa215b9bd
Xavier Watkins <xavier.watkins@gmail.com>
2019-03-11 10:46:04 +0000

    Adding custom 404 redirect for gh-pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -11,6 +12,10 @@ module.exports = merge(common, {
     filename: 'app.[hash].bundle.js',
   },
   plugins: [
+    new HtmlWebPackPlugin({
+      template: `${__dirname}/404.html`,
+      filename: '404.html',
+    }),
     // Minify CSS
     new webpack.LoaderOptionsPlugin({
       minimize: true,
