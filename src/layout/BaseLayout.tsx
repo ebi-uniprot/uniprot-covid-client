ef599a92d11971a5e89217a17a4e5e5542be70e3
Xavier Watkins <xavier.watkins@gmail.com>
2019-03-11 16:00:10 +0000

    Adding bug submission link

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -8,11 +8,32 @@ type BaseLayoutProps = {
   isHomePage?: boolean;
 };
 
-const BaseLayout: React.FC<BaseLayoutProps> = (props) => {
+const style: React.CSSProperties = {
+  fontSize: '.8rem',
+  lineHeight: '1.5rem',
+  display: 'block',
+  padding: '.5rem 0',
+  color: '#FFFFFF',
+  backgroundColor: 'red',
+  position: 'fixed',
+  bottom: '4rem',
+  right: 0,
+  writingMode: 'vertical-rl',
+  textOrientation: 'sideways',
+};
+
+const BaseLayout: React.FC<BaseLayoutProps> = props => {
   const { children, isHomePage } = props;
   return (
     <section id="outer-layout">
       <UniProtHeader isHomePage={isHomePage} />
+      <a
+        style={style}
+        target="_blank"
+        href="https://goo.gl/forms/VrAGbqg2XFg6Mpbh1"
+      >
+        Report bug
+      </a>
       {children}
       <UniProtFooter />
     </section>
