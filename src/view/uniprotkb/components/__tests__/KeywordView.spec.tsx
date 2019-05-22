f9873216f6a909b42a80f002e83e522fc77361e9
Xavier Watkins <xavier.watkins@gmail.com>
2019-05-22 15:09:43 +0100

    🚀 Tests passing 🚀

--- src/view/uniprotkb/components/__tests__/KeywordView.spec.tsx
+++ src/view/uniprotkb/components/__tests__/KeywordView.spec.tsx
@@ -1,39 +1,18 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { KeywordItem, KeywordList, Keyword } from '../Keyword';
-import data from './modelData.json';
-import EntrySectionType from '../types/EntrySection';
+import { render, cleanup } from 'react-testing-library';
+import { MemoryRouter as Router } from 'react-router-dom';
+import KeywordView from '../KeywordView';
+import KeywordUIDataJson from '../__mocks__/KeywordUIData.json';
 
-configure({ adapter: new Adapter() });
+afterAll(() => cleanup());
 
 describe('Keyword', () => {
-  for (const section in EntrySectionType) {
-    test(`should render for ${section} section`, () => {
-      const wrapper = shallow(
-        <Keyword data={data} section={section as EntrySectionType} />
-      );
-      expect(wrapper.debug()).toMatchSnapshot();
-    });
-  }
-});
-
-describe('KeywordList', () => {
-  test('should render for database category GENOME and Ensembl', () => {
-    const { keywords } = data;
-    const wrapper = shallow(<KeywordList keywords={keywords} />);
-    expect(wrapper.debug()).toMatchSnapshot();
+  test(`should render Keywords for section`, () => {
+    const { asFragment } = render(
+      <Router>
+        <KeywordView keywords={KeywordUIDataJson} />
+      </Router>
+    );
+    expect(asFragment()).toMatchSnapshot();
   });
-});
-
-describe('KeywordItem', () => {
-  const { keywords } = data;
-  for (const keyword of keywords) {
-    test('should render for database Ensembl', () => {
-      const wrapper = shallow(
-        <KeywordItem id={keyword.id} value={keyword.value} />
-      );
-      expect(wrapper.debug()).toMatchSnapshot();
-    });
-  }
 });
