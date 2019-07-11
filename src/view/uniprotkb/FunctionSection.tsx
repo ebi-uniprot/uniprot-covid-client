d8e3d49e953e461ae400f60f323be85e81bddbdc
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-07-11 10:47:40 +0100

    Remove header from AGR's ribbon component and place above to demarcate GO annotation section; ribbon scrolls horizontally; minor styling for section.

--- src/view/uniprotkb/FunctionSection.tsx
+++ src/view/uniprotkb/FunctionSection.tsx
@@ -6,47 +6,35 @@ import KeywordView from '../uniprotkb/components/KeywordView';
 import XRefView from '../uniprotkb/components/XRefView';
 import FeaturesView from '../uniprotkb/components/FeaturesView';
 import EntrySection from '../../model/types/EntrySection';
-import { hasContent } from '../../model/utils/utils';
 import Comment from '../../model/types/Comment';
 import { UIModel } from '../../model/uniprotkb/SectionConverter';
-import GORibbon from '../uniprotkb/components/GORibbon';
-
-const subject = 'MGI:MGI:97490';
-const mode = undefined;
-const entityLabel = 2;
-const colorBy = 0;
-const binaryColor = false;
+import GoRibbon from './components/GoRibbon';
 
 const FunctionSection: FC<{
   data: UIModel;
   sequence: string;
   primaryAccession: string;
-}> = ({ data, sequence, primaryAccession }) => {
-  if (!hasContent(data)) {
-    return null;
-  }
-  return (
-    <Fragment>
-      <Card title={EntrySection.Function}>
-        <FreeTextView comments={data.commentsData.get(Comment.FUNCTION)} />
-        <CatalyticActivityView
-          comments={data.commentsData.get(Comment.CATALYTIC_ACTIVITY)}
-        />
-        <FreeTextView
-          comments={data.commentsData.get(Comment.PATHWAY)}
-          includeTitle={true}
-        />
-        <FreeTextView
-          comments={data.commentsData.get(Comment.MISCELLANEOUS)}
-          includeTitle={true}
-        />
-        <FeaturesView features={data.featuresData} sequence={sequence} />
-        <GORibbon primaryAccession={primaryAccession} />
-        <KeywordView keywords={data.keywordData} />
-        <XRefView xrefs={data.xrefData} primaryAccession={primaryAccession} />
-      </Card>
-    </Fragment>
-  );
-};
+}> = ({ data, sequence, primaryAccession }) => (
+  <Fragment>
+    <Card title={EntrySection.Function}>
+      <FreeTextView comments={data.commentsData.get(Comment.FUNCTION)} />
+      <CatalyticActivityView
+        comments={data.commentsData.get(Comment.CATALYTIC_ACTIVITY)}
+      />
+      <FreeTextView
+        comments={data.commentsData.get(Comment.PATHWAY)}
+        includeTitle={true}
+      />
+      <FreeTextView
+        comments={data.commentsData.get(Comment.MISCELLANEOUS)}
+        includeTitle={true}
+      />
+      <FeaturesView features={data.featuresData} sequence={sequence} />
+      <GoRibbon primaryAccession={primaryAccession} />
+      <KeywordView keywords={data.keywordData} />
+      <XRefView xrefs={data.xrefData} primaryAccession={primaryAccession} />
+    </Card>
+  </Fragment>
+);
 
 export default FunctionSection;
