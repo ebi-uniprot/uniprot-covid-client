a40ce980b98eba9d310feb3d899574a04aef1bc6
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-07-17 14:41:31 +0100

    utils.ts lint clean up.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,14 @@
-export const serializableDeepCopy = (x: any) => JSON.parse(JSON.stringify(x));
+export const serializableDeepCopy = (x: object) =>
+  JSON.parse(JSON.stringify(x));
 
-export const serializableDeepAreEqual = (obj1: any, obj2: any) =>
+export const serializableDeepAreEqual = (obj1: object, obj2: object) =>
   JSON.stringify(obj1) === JSON.stringify(obj2);
 
 export const removeProperty = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   obj: { [key: string]: any },
-  property: any
-): { [key: string]: any } => {
+  property: string | number
+): { [key: string]: object } => {
   const { [property]: unwantedProperty, ...objWithoutProperty } = obj;
   return objWithoutProperty;
 };
@@ -15,11 +17,13 @@ export const formatLargeNumber = (x: number) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
-export const loadWebComponent = (name: string, className: any) => {
+export const loadWebComponent = (name: string, className: Function) => {
   if (window.customElements && !window.customElements.get(name)) {
     window.customElements.define(name, className);
   }
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const flattenArrays = (arrays: any[][]) =>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [].concat(...(arrays as any[]));
