27f2d3e3143ff77e5c90fa4afbca7282ac8d515f
Xavier Watkins <xavier.watkins@gmail.com>
2019-09-11 17:16:13 +0100

    Add test for summary

---
+++ src/view/uniprotkb/summary/__tests__/ProteinSummary.spec.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, cleanup, waitForElement } from '@testing-library/react';
+import axios from 'axios';
+import { MemoryRouter as Router } from 'react-router-dom';
+import ProteinSummary from '../ProteinSummary';
+import modelData from '../../../../model/__mocks__/modelData.json';
+
+afterEach(cleanup);
+
+jest.mock('axios');
+
+axios.get.mockResolvedValue({
+  data: modelData,
+});
+
+describe('ProteinSummary component', () => {
+  test('it renders without crashing', async () => {
+    const { asFragment, getByText } = render(
+      <Router>
+        <ProteinSummary accession="P05067" />
+      </Router>
+    );
+    const resolved = await waitForElement(() => getByText('P21802'));
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
