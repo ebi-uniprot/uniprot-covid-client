a302babff68f177213e59f6bd4930f2c001e5106
Xavier Watkins <xavier.watkins@gmail.com>
2019-09-17 14:50:16 +0100

    Update tests

--- src/results/utils/__tests__/utils.spec.ts
+++ src/results/utils/__tests__/utils.spec.ts
@@ -8,7 +8,7 @@ describe('utils', () => {
     ];
     const queryString = getAPIQueryUrl('cdc7', [], facets);
     expect(queryString).toBe(
-      'https://wwwdev.ebi.ac.uk/uniprot/api/uniprotkb/search?facets=reviewed%2Cpopular_organism%2Cother_organism&fields=&query=cdc7%20AND%20%28facet1%3Avalue%201%29%20AND%20%28facet2%3Avalue%203%29'
+      'https://wwwdev.ebi.ac.uk/uniprot/api/uniprotkb/search?facets=reviewed%2Cpopular_organism%2Cother_organism%2Clength%2Cproteins_with&fields=&query=cdc7%20AND%20%28facet1%3A%22value%201%22%29%20AND%20%28facet2%3A%22value%203%22%29'
     );
   });
 
@@ -16,8 +16,12 @@ describe('utils', () => {
     const facets = [
       { name: 'facet1', value: 'value 1' },
       { name: 'facet2', value: 'value 3' },
+      { name: 'facet3', value: 'value3' },
+      { name: 'facet4', value: '[1 TO *]' },
     ];
     const queryString = createFacetsQueryString(facets);
-    expect(queryString).toBe(' AND (facet1:value 1) AND (facet2:value 3)');
+    expect(queryString).toBe(
+      ' AND (facet1:"value 1") AND (facet2:"value 3") AND (facet3:value3) AND (facet4:[1 TO *])'
+    );
   });
 });
