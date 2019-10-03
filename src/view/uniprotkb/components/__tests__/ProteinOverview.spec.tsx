bd4d46a57635a7851ce1410d62a58819cb88486a
Daniel Rice <dlrice@ebi.ac.uk>
2019-10-03 18:25:34 +0100

    Test annotationScoreToPercentage.

--- src/view/uniprotkb/components/__tests__/ProteinOverview.spec.tsx
+++ src/view/uniprotkb/components/__tests__/ProteinOverview.spec.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import ProteinOverview from '../ProteinOverviewView';
+import ProteinOverview, {
+  annotationScoreToPercentage,
+} from '../ProteinOverviewView';
 import ProteinNamesUIDataJson from '../__mocks__/ProteinNamesUIData.json';
 import EntrySection from '../../../../model/types/EntrySection';
 
@@ -16,5 +18,35 @@ describe('ProteinOverview component', () => {
       <ProteinOverview transformedData={transformedData} />
     );
     expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe('annotationScoreToPercentage function', () => {
+  /*
+    0-19  | bin 1 | 20%
+    20-39 | bin 2 | 40%
+    40-59 | bin 3 | 60%
+    60-79 | bin 4 | 80%
+    >=80  | bin 5 | 100%
+  */
+  const annotationScoreToPercentageTests = [
+    // [annotation score, percentage]
+    [0, 20],
+    [1, 20],
+    [19, 20],
+    [20, 40],
+    [39, 40],
+    [40, 60],
+    [59, 60],
+    [60, 80],
+    [79, 80],
+    [80, 100],
+    [81, 100],
+    [1000, 100],
+  ];
+  annotationScoreToPercentageTests.forEach(([annotationScore, percentage]) => {
+    test(`Should convert annotationScore of ${annotationScore} to ${percentage}%`, () => {
+      expect(annotationScoreToPercentage(annotationScore)).toEqual(percentage);
+    });
   });
 });
