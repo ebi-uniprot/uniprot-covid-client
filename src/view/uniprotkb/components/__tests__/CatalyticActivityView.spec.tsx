946f828c15d2b20d69ff30913b29ce3e6edbb7df
Daniel Rice <dlrice@ebi.ac.uk>
2019-10-03 14:43:07 +0100

    Write rhea viz tests and snapshot.

--- src/view/uniprotkb/components/__tests__/CatalyticActivityView.spec.tsx
+++ src/view/uniprotkb/components/__tests__/CatalyticActivityView.spec.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import CatalyticActivityView from '../CatalyticActivityView';
+import CatalyticActivityView, {
+  getRheaId,
+  isRheaReactReference,
+  RheaReactionVisualizer,
+} from '../CatalyticActivityView';
 import CatalyticActivityUIDataJson from '../__mocks__/CatalyticActivityUIData.json';
 
 describe('Catalytic activity', () => {
@@ -9,5 +13,36 @@ describe('Catalytic activity', () => {
       <CatalyticActivityView comments={CatalyticActivityUIDataJson} />
     );
     expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe('RheaReactionVisualizer', () => {
+  test('should render RheaReactionVisualizer', () => {
+    const { asFragment } = render(
+      <RheaReactionVisualizer rheaId="12345" show={false} />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe('getRheaId', () => {
+  test('should return id 12345 from RHEA:12345', () => {
+    expect(getRheaId('RHEA:12345')).toEqual('12345');
+  });
+  test('should return null from RHEA-COMP:12345', () => {
+    expect(getRheaId('RHEA-COMP:12345')).toEqual(null);
+  });
+});
+
+describe('isRheaReactReference', () => {
+  test('should return true when database=Rhea and id string is RHEA:12345', () => {
+    expect(
+      isRheaReactReference({ databaseType: 'Rhea', id: 'RHEA:12345' })
+    ).toEqual(true);
+  });
+  test('should return true when database=ChEBI and id string is CHEBI:57287', () => {
+    expect(
+      isRheaReactReference({ databaseType: 'ChEBI', id: 'CHEBI:57287' })
+    ).toEqual(false);
   });
 });
