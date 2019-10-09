8669f311804ecea4014ea9dd252cc65c80cd8078
Xavier Watkins <xavier.watkins@gmail.com>
2019-10-09 15:18:49 +0100

    Update tests

--- src/search/__tests__/TextField.spec.tsx
+++ src/search/__tests__/TextField.spec.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { render, fireEvent } from '@testing-library/react';
 import TextField from '../TextField';
 
-configure({ adapter: new Adapter() });
-let props;
-let wrapper;
-
 describe('Range field', () => {
-  beforeEach(() => {
-    props = {
-      field: {
-        label: 'UniProtKB AC',
-        itemType: 'single',
-        term: 'accession',
-        description: 'Search by UniProtKB Accession',
-        example: 'P12345',
-      },
-      type: 'string',
-      queryInput: {},
-      handleChange: jest.fn(),
-    };
-    wrapper = shallow(<TextField {...props} />);
-  });
+  const props = {
+    field: {
+      label: 'UniProtKB AC',
+      itemType: 'single',
+      term: 'accession',
+      description: 'Search by UniProtKB Accession',
+      example: 'P12345',
+    },
+    type: 'string',
+    queryInput: {},
+    handleChange: jest.fn(),
+  };
 
   test('should render a text field', () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  test('should handle change', () => {
-    wrapper.find(`#input_${props.field.term}`).simulate('change', { target: { value: 'val1' } });
-    expect(props.handleChange).toHaveBeenCalled();
+    const { asFragment, getByPlaceholderText } = render(
+      <TextField {...props} />
+    );
+    const inputElt = getByPlaceholderText('P12345');
+    expect(inputElt.value).toBe('');
+    fireEvent.change(inputElt, { target: { value: 'Some term' } });
+    expect(inputElt.value).toBe('Some term');
+    expect(asFragment()).toMatchSnapshot();
   });
 });
