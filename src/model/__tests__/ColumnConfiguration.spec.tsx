60ee0b620b151e1a1e703437ad3ba20e2940d861
Xavier Watkins <xavier.watkins@gmail.com>
2019-10-16 11:18:50 +0100

    More generic testing strategy for columns

--- src/model/__tests__/ColumnConfiguration.spec.tsx
+++ src/model/__tests__/ColumnConfiguration.spec.tsx
@@ -12,42 +12,12 @@ describe('ColumnConfiguration component', () => {
     transformedData = uniProtKbConverter(data);
   });
 
-  test('should render accession', () => {
-    const { asFragment } = render(
-      <MemoryRouter>
-        {ColumnConfiguration.accession.render(transformedData)}
-      </MemoryRouter>
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('should render id', () => {
-    const { asFragment } = render(
-      ColumnConfiguration.id.render(transformedData)
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('should render protein_name', () => {
-    const { asFragment } = render(
-      ColumnConfiguration.protein_name.render(transformedData)
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('should render gene_names', () => {
-    const { asFragment } = render(
-      ColumnConfiguration.gene_names.render(transformedData)
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('should render organism', () => {
-    const { asFragment } = render(
-      <MemoryRouter>
-        {ColumnConfiguration.organism.render(transformedData)}
-      </MemoryRouter>
-    );
-    expect(asFragment()).toMatchSnapshot();
+  test('should render all columns', () => {
+    Object.values(ColumnConfiguration).forEach(column => {
+      const { asFragment } = render(
+        <MemoryRouter>{column.render(transformedData)}</MemoryRouter>
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
   });
 });
