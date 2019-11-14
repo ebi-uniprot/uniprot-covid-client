a661e9a66a47f34a86cfd9ce5db8aae2132180c5
Xavier Watkins <xwatkins@ebi.ac.uk>
2019-11-14 11:54:37 +0000

    Add interaction visualisation and table (#84)
    * Add interaction visualisation and table
    
    * Add check for empty array
    
    * Simplify return

---
+++ src/types/interaction-viewer.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'interaction-viewer';
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'interaction-viewer': any;
+  }
+}
