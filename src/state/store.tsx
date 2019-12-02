eed5c5bde376394503082b71d1615ed8410c633c
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2019-12-02 15:02:01 +0000

    redux persistant storage with LocalStorage

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,14 +1,38 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import { createWhitelistFilter, createBlacklistFilter } from 'redux-persist-transform-filter';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from './rootReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+  transforms: [
+    createWhitelistFilter(
+      'results',
+      ['tableColumns', 'viewMode'],  // save
+      ['tableColumns', 'viewMode'],  // load
+    ),
+    createBlacklistFilter(
+      'query',
+      ['clauses', 'queryString', 'namespace', 'searchTerms', 'evidences'], // ignore
+    ),
+  ],
+};
+
+const persistorReducer = persistReducer(persistConfig, rootReducer);
 
 function configureStore(initialState?: object) {
   let store;
+
   if (process.env.NODE_ENV === 'development') {
     const debug = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
     store = createStore(
-      rootReducer,
+      persistorReducer,
       initialState,
       debug
         ? debug(applyMiddleware(thunkMiddleware))
@@ -16,7 +40,7 @@ function configureStore(initialState?: object) {
     );
   } else {
     store = createStore(
-      rootReducer,
+      persistorReducer,
       initialState,
       applyMiddleware(thunkMiddleware)
     );
@@ -24,6 +48,7 @@ function configureStore(initialState?: object) {
   return store;
 }
 
-const store = configureStore();
+export const store = configureStore();
+export const persistor = persistStore(store);
 
 export default store;
