bfead6704febc44022252fceec4c6eca2bb1d0bd
Xavier Watkins <xavier.watkins@gmail.com>
2019-12-09 13:57:17 +0000

    Fix tests

--- src/view/uniprotkb/components/UniProtTitle.tsx
+++ src/view/uniprotkb/components/UniProtTitle.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC } from 'react';
 import { SwissProtIcon, TremblIcon } from 'franklin-sites';
 import { Link } from 'react-router-dom';
 import { EntryType } from '../../../model/uniprotkb/UniProtkbConverter';
