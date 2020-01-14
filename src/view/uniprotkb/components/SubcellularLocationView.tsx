9776d3c6e566831fcd48521a232fb918a33142ba
Xavier Watkins <xavier.watkins@gmail.com>
2020-01-14 11:08:47 +0000

    logic was wrong

--- src/view/uniprotkb/components/SubcellularLocationView.tsx
+++ src/view/uniprotkb/components/SubcellularLocationView.tsx
@@ -7,7 +7,7 @@ import { TextView } from './FreeTextView';
 const SubcellularLocationView: FC<{
   comments?: SubcellularLocationComment[];
 }> = ({ comments }) => {
-  if (!comments || comments.length) {
+  if (!comments || !comments.length) {
     return null;
   }
   return (
