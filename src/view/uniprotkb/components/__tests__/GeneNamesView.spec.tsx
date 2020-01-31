c0f8fd8a4ffc0ee1c1f27120b35861c0c0312caf
Xavier Watkins <xavier.watkins@gmail.com>
2020-01-31 15:38:27 +0000

    🩹Fix tests

--- src/view/uniprotkb/components/__tests__/GeneNamesView.spec.tsx
+++ src/view/uniprotkb/components/__tests__/GeneNamesView.spec.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import GeneNamesView from '../GeneNamesView';
+import renderWithRedux from '../../../../__testHelpers__/renderWithRedux';
 import GeneNamesUIDataJson from '../__mocks__/GeneNamesUIData.json';
 
 describe('GeneNames', () => {
   test('should render gene_names', () => {
-    const { asFragment } = render(
+    const { asFragment } = renderWithRedux(
       <GeneNamesView geneNamesData={GeneNamesUIDataJson} />
     );
     expect(asFragment()).toMatchSnapshot();
