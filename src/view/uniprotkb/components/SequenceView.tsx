d9698343587f65531633028b93ecb0719bc788d9
Daniel Rice <dlrice@ebi.ac.uk>
2020-03-27 17:05:28 +0000

    Update Sequence to use downloadUrl prop.

--- src/view/uniprotkb/components/SequenceView.tsx
+++ src/view/uniprotkb/components/SequenceView.tsx
@@ -84,7 +84,11 @@ export const SequenceInfo: React.FC<{
   return (
     <Fragment>
       {dataToDisplay && <InfoList infoData={infoData} />}
-      <Sequence sequence={dataToDisplay.value} accession={isoformId} />
+      <Sequence
+        sequence={dataToDisplay.value}
+        accession={isoformId}
+        downloadUrl={apiUrls.sequenceFasta(isoformId)}
+      />
     </Fragment>
   );
 };
