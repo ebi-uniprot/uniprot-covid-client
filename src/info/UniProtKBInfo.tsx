541c496c3f2a9b7581dc00582bb5a7b0a0baaaf6
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2020-03-27 16:28:59 +0000

    now the ftp link in the page intro opens in the same page to match the uniprot website behaviour

--- src/info/UniProtKBInfo.tsx
+++ src/info/UniProtKBInfo.tsx
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const UniProtKBInfo = () => (
   <Fragment>
     <p>
-      This page lists the latest available UniProtKB data relating to the COVID-19 outbreak, including protein entries for the Sars-Cov-2 coronavirus. It will be updated as new information becomes available, independent of the general UniProt release schedule. Ths data can also be via our FTP on <a href="ftp://ftp.uniprot.org/pub/databases/uniprot/pre_release/" target="_blank">ftp://ftp.uniprot.org/pub/databases/uniprot/pre_release/</a>.
+      This page lists the latest available UniProtKB data relating to the COVID-19 outbreak, including protein entries for the Sars-Cov-2 coronavirus. It will be updated as new information becomes available, independent of the general UniProt release schedule. Ths data can also be via our FTP on <a href="ftp://ftp.uniprot.org/pub/databases/uniprot/pre_release/">ftp://ftp.uniprot.org/pub/databases/uniprot/pre_release/</a>.
     </p>
   </Fragment>
 );
