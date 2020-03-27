c425dfdc29feec61dd236f033aa78d18ccf7779e
Xavier Watkins <xavier.watkins@gmail.com>
2020-03-27 14:35:28 +0000

    update config

--- webpack.staging.js
+++ webpack.staging.js
@@ -12,7 +12,7 @@ module.exports = merge(common, {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'build'),
-    publicPath: '/uniprot-covid-client',
+    publicPath,
     filename: 'app.[hash].bundle.js',
   },
   plugins: [
