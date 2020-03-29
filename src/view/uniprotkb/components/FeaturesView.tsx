60149c7dbb8a45d07ebfe323ad6077d4f7480db2
Xavier Watkins <xavier.watkins@gmail.com>
2020-03-29 15:11:34 +0100

    Add feature id

--- src/view/uniprotkb/components/FeaturesView.tsx
+++ src/view/uniprotkb/components/FeaturesView.tsx
@@ -41,6 +41,7 @@ export type FeatureData = {
 }[];
 
 export type ProtvistaFeature = {
+  accession?: string;
   type: string;
   description: string;
   evidences: Evidence[];
@@ -55,6 +56,7 @@ type FeatureProps = {
   features: FeatureData;
 };
 
+// TODO: looks like this could be merged with ProtvistaFeature?
 export type ProcessedFeature = {
   accession: string | undefined;
   start: number;
@@ -111,6 +113,10 @@ const FeaturesView: React.FC<FeatureProps> = ({
           ${d.evidences &&
             UniProtProtvistaEvidenceTag(d.evidences, evidenceTagCallback)}
         `,
+    },
+    featureId: {
+      label: 'ID',
+      resolver: (d: ProtvistaFeature) => (d.accession ? d.accession : '-'),
     },
   });
 
