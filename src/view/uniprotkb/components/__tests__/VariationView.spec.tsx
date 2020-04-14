14ab910a319fafcbed7fff01db481c3708c6b2df
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-04-14 15:05:12 +0100

    change UI tests to cover more cases

--- src/view/uniprotkb/components/__tests__/VariationView.spec.tsx
+++ src/view/uniprotkb/components/__tests__/VariationView.spec.tsx
@@ -1,20 +1,61 @@
 import React from 'react';
-import axios from 'axios';
 import { render, cleanup } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
+import useDataApi from '../../../../hooks/useDataApi';
+
 import VariationView from '../VariationView';
 
-jest.mock('axios');
-const resp = { data: { sequence: 'ABCDEFG', features: [{}, {}] } };
+jest.mock('../../../../hooks/useDataApi');
+
 afterEach(cleanup);
 
 describe('VariationView component', () => {
-  test('it renders without crashing', async () => {
-    axios.get.mockResolvedValue(resp);
-    axios.CancelToken.source.mockImplementation(() => ({ cancel: () => {} }));
+  test('renders on loading', async () => {
+    useDataApi.mockReturnValue({ loading: true });
     await act(async () => {
       const { asFragment } = render(
         <VariationView primaryAccession="P05067" />
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+
+  test('renders on error', async () => {
+    useDataApi.mockReturnValue({
+      loading: false,
+      error: new Error('some error'),
+      status: 500,
+    });
+    await act(async () => {
+      const { asFragment } = render(
+        <VariationView primaryAccession="P05067" />
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+
+  test('renders on no data', async () => {
+    useDataApi.mockReturnValue({
+      loading: false,
+      error: new Error('some error'),
+      status: 404,
+    });
+    await act(async () => {
+      const { asFragment } = render(
+        <VariationView primaryAccession="P05067" />
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+
+  test('renders on data', async () => {
+    useDataApi.mockReturnValue({
+      loading: false,
+      data: { sequence: 'ABCDEFG', features: [{}, {}] },
+    });
+    await act(async () => {
+      const { asFragment } = render(
+        <VariationView primaryAccession="P05067" title="some title" />
       );
       expect(asFragment()).toMatchSnapshot();
     });
