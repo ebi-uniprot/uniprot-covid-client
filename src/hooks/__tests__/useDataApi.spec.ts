69901cd0931bbb69b72123b7ec1b08da5ea1881c
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-04-14 16:04:50 +0100

    forgot to remove code comment

--- src/hooks/__tests__/useDataApi.spec.ts
+++ src/hooks/__tests__/useDataApi.spec.ts
@@ -9,21 +9,6 @@ let mock;
 
 beforeAll(() => {
   mock = new MockAdapter(axios);
-  // test('useFetch performs GET request', async () => {
-  // const initialValue = [];
-  // const mock = new MockAdapter(axios);
-  // const mockData = "response";
-  // const url = "http://mock";
-  // mock.onGet(url).reply(200, mockData);
-  // const { result, waitForNextUpdate } = renderHook(() =>
-  //   useFetch(url, initialValue)
-  // );
-  // expect(result.current.data).toEqual([]);
-  // expect(result.current.loading).toBeTruthy();
-  // await waitForNextUpdate();
-  // expect(result.current.data).toEqual("response");
-  // expect(result.current.loading).toBeFalsy();
-  // });
 });
 
 afterEach(() => {
