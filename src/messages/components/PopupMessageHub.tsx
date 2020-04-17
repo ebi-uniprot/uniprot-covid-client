a4fe5a9d9c92bbc9a5b2b9b22d443752d4e98416
Xavier Watkins <xavier.watkins@gmail.com>
2020-04-17 09:10:18 +0100

    Simplify animation

--- src/messages/components/PopupMessageHub.tsx
+++ src/messages/components/PopupMessageHub.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createRef } from 'react';
+import React from 'react';
 import { Message } from 'franklin-sites';
 import { useTransition, animated } from 'react-spring';
 import { MessageType } from '../types/messagesTypes';
@@ -7,30 +7,22 @@ import '../styles/popupMessageHub.scss';
 const PopUpMessageHub: React.FC<{ messages: MessageType[] }> = ({
   messages,
 }) => {
-  // const [refMap, setRefMap] = useState(() => new WeakMap());
-
   const transitions = useTransition(messages, (item) => item.id, {
     from: { opacity: 0 },
-    enter: (item) => async (next) =>
-      await next({
-        opacity: 1,
-      }),
+    enter: {
+      opacity: 1,
+    },
+    exit: {
+      opacity: 0,
+    },
   });
-
-  // useEffect(() => {
-  //   const refs = new WeakMap();
-  //   messages.forEach((message) => refs.set(message, createRef));
-  //   setRefMap(refs);
-  // }, []);
-
-  console.log(transitions);
 
   return (
     <div className="popup-message-container">
-      {transitions.map(({ key, item: message, props: { ...style } }) => {
+      {transitions.map(({ key, item, props }) => {
         return (
-          <animated.div key={key} style={style}>
-            <Message level={message.level}>{message.content}</Message>
+          <animated.div key={key} style={props}>
+            <Message level={item.level}>{item.content}</Message>
           </animated.div>
         );
       })}
