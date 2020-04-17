5eaf59499b0d735f74961c58cd508721b3fae7d7
Xavier Watkins <xavier.watkins@gmail.com>
2020-04-17 09:24:49 +0100

    Adding automatic message addition for testing

--- src/messages/MessageManagerContainer.tsx
+++ src/messages/MessageManagerContainer.tsx
@@ -1,9 +1,14 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { v1 } from 'uuid';
 import { bindActionCreators, Dispatch } from 'redux';
 import { groupBy } from 'lodash';
 import * as actions from './state/messagesActions';
-import { MessageType, MessageFormat } from './types/messagesTypes';
+import {
+  MessageType,
+  MessageFormat,
+  MessageLevel,
+} from './types/messagesTypes';
 import { RootAction, RootState } from '../state/state-types';
 import InPageMessageHub from './components/InPageMessageHub';
 import PopUpMessageHub from './components/PopupMessageHub';
@@ -26,6 +31,19 @@ const MessageManager: FC<MessageManagerContainerProps> = ({
     ({ format }) => format
   );
   console.log(activeMessages, inPageMessages, popUpMessages);
+  // Temporary add messages to test pop ups are working
+  useEffect(() => {
+    const interval = setInterval(() => {
+      addMessage({
+        id: v1(),
+        content: `New message ${v1()}`,
+        format: MessageFormat.POP_UP,
+        level: MessageLevel.INFO,
+      });
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [addMessage]);
+
   return (
     <Fragment>
       <InPageMessageHub
