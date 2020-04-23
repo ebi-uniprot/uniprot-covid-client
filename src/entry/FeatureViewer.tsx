975d8dbdc6379c339ddcafbf8172e0ffc18d8d18
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-04-23 19:21:44 +0100

    update random stuff, variation still broken

--- src/entry/FeatureViewer.tsx
+++ src/entry/FeatureViewer.tsx
@@ -1,61 +1,58 @@
-import React, { FC, useRef, useEffect, useState } from 'react';
-import ProtvistaUniprot from // transformDataFeatureAdapter,
-// transformDataProteomicsAdapter,
-// transformDataStructureAdapter,
-// transformDataVariationAdapter,
+import React, { FC /* useRef, useEffect, useState */ } from 'react';
+import ProtvistaUniprot from // transformDataVariationAdapter, // transformDataStructureAdapter, // transformDataProteomicsAdapter, // transformDataFeatureAdapter,
 // transformDataInterproAdapter,
 'protvista-uniprot';
 
-import useDataApi from '../utils/useDataApi';
+// import useDataApi from '../utils/useDataApi';
 import { loadWebComponent } from '../utils/utils';
 
-const transformData = (data: any) => {
-  const output: any = {};
+// const transformData = (data: any) => {
+//   const output: any = {};
 
-  // output.STRUCTURE = transformDataStructureAdapter(data);
+//   // output.STRUCTURE = transformDataStructureAdapter(data);
 
-  return output;
-};
+//   return output;
+// };
 
 loadWebComponent('protvista-uniprot', ProtvistaUniprot);
 
 const FeatureViewer: FC<{ accession: string }> = ({ accession }) => {
-  const viewerRef: any = useRef();
-  // not false, otherwise will render as "false" in the DOM, we need undefined
-  const [suspended, setSuspended] = useState<true | undefined>(undefined);
-  const data = useDataApi(
-    `https://www.ebi.ac.uk/uniprot/api/covid-19/uniprotkb/accession/${accession}`
-  );
+  // const viewerRef: any = useRef();
+  // // not false, otherwise will render as "false" in the DOM, we need undefined
+  // const [suspended, setSuspended] = useState<true | undefined>(undefined);
+  // const data = useDataApi(
+  //   `https://www.ebi.ac.uk/uniprot/api/covid-19/uniprotkb/accession/${accession}`
+  // );
 
-  useEffect(() => {
-    if (!data.sequence || !viewerRef.current) return;
+  // useEffect(() => {
+  //   if (!data.sequence || !viewerRef.current) return;
 
-    let mounted = true;
-    customElements.whenDefined('protvista-uniprot').then(() => {
-      if (!mounted || !data.sequence || !viewerRef.current) return;
-      // do stuff
-      console.log(data);
-      viewerRef.current.sequence = data.sequence.value;
-      viewerRef.current.displayCoordinates = {
-        start: 1,
-        end: data.sequence.length,
-      };
-      viewerRef.current.data = transformData(data);
-      setSuspended(undefined);
-    });
-    // eslint-disable-next-line consistent-return
-    return () => {
-      mounted = false;
-    };
-  }, [data]);
+  //   let mounted = true;
+  //   customElements.whenDefined('protvista-uniprot').then(() => {
+  //     if (!mounted || !data.sequence || !viewerRef.current) return;
+  //     // do stuff
+  //     console.log(data);
+  //     viewerRef.current.sequence = data.sequence.value;
+  //     viewerRef.current.displayCoordinates = {
+  //       start: 1,
+  //       end: data.sequence.length,
+  //     };
+  //     viewerRef.current.data = transformData(data);
+  //     setSuspended(undefined);
+  //   });
+  //   // eslint-disable-next-line consistent-return
+  //   return () => {
+  //     mounted = false;
+  //   };
+  // }, [data]);
 
-  if (!data.sequence) return null;
+  // if (!data.sequence) return null;
 
   return (
     <section>
       <div style={{ height: '80vh' }}>
         <h2>ProtVista visualisation for {accession}</h2>
-        <protvista-uniprot suspend={suspended} ref={viewerRef} />
+        <protvista-uniprot accession={accession} />
       </div>
     </section>
   );
--- webpack.common.js
+++ webpack.common.js
@@ -17,10 +17,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx|tsx|ts)$/,
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, 'node_modules/protvista-uniprot'),
-        ],
+        include: [path.resolve(__dirname, 'src')],
         use: {
           loader: 'babel-loader',
         },
