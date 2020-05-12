710477e3cd45974a314df225395745b78681912b
Xavier Watkins <xavier.watkins@gmail.com>
2020-05-12 17:30:11 +0100

    Update webpack config to work with linked franklin

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,12 @@ module.exports = (_env, argv) => {
     })(),
     resolve: {
       extensions: ['.tsx', '.jsx', '.js', '.ts'],
+      alias: {
+        react: path.resolve('./node_modules/react'),
+        'react-dom': path.resolve('./node_modules/react-dom'),
+        'react-router-dom': path.resolve('./node_modules/react-router-dom'),
+      },
+      symlinks: false,
     },
     // MODULE
     module: {
