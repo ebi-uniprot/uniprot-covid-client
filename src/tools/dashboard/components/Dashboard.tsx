f3b74a61a4879dad83b5202394c3501434055d6c
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-05-27 17:35:52 +0200

    added basic dashboard

---
+++ src/tools/dashboard/components/Dashboard.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Card } from 'franklin-sites';
+
+import { Job } from '../../blast/types/blastJob';
+
+import Row from './Row';
+
+import './styles/Dashboard.scss';
+
+// temporary
+import created from '../../blast/__mocks__/internal-jobs/created';
+import failedBeforeSubmission from '../../blast/__mocks__/internal-jobs/failed-after-submission';
+import failedAfterSubmission from '../../blast/__mocks__/internal-jobs/failed-before-submission copy';
+import finished from '../../blast/__mocks__/internal-jobs/finished';
+import running from '../../blast/__mocks__/internal-jobs/running';
+
+const Dashboard = () => {
+  // const tools: Array<Job> = useSelector((state) => Object.values(state.tools));
+  const tools: Array<Job> = [
+    created,
+    failedBeforeSubmission,
+    failedAfterSubmission,
+    finished,
+    running,
+  ];
+
+  return (
+    <section>
+      <p>
+        Your tool analysis results from the last 7 days are listed below. For
+        any tools still running, you can navigate away to other pages and will
+        be notified once the job is finished.
+      </p>
+      <div className="dashboard">
+        <div className="dashboard__header">
+          <Card>
+            <span>Name</span>
+            <span>Job type</span>
+            <span>Created</span>
+            <span>Status</span>
+          </Card>
+        </div>
+        <div className="dashboard__body">
+          {tools.map((tool) => (
+            <Row tool={tool} key={tool.internalID} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Dashboard;
