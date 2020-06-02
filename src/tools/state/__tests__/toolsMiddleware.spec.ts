2a1807e85a9cd1366a2503afb5cc988ce2961845
Xavier Watkins <xavier.watkins@gmail.com>
2020-06-02 18:02:39 +0100

    First attempt at middleware testing

--- src/tools/state/__tests__/toolsMiddleware.spec.ts
+++ src/tools/state/__tests__/toolsMiddleware.spec.ts
@@ -1,13 +1,35 @@
-import toolsMiddleware from '../toolsMiddleware';
+import { schedule, sleep } from 'timing-functions';
 import { action } from 'typesafe-actions';
+import postData from '../../../uniprotkb/config/postData';
+
+import toolsMiddleware from '../toolsMiddleware';
+import createdJob from '../../blast/__mocks__/internal-jobs/created';
 import JobStore from '../../utils/storage';
+import { REHYDRATE_JOBS } from '../toolsActions';
+
+jest.mock('timing-functions');
 
 jest.mock('../../utils/storage', () => jest.fn());
+jest.mock('../../../uniprotkb/config/postData', () =>
+  Promise.resolve({ data: 'some-id' })
+);
 
 const next = jest.fn();
-const store = jest.fn();
+const store = {
+  dispatch: jest.fn(),
+  getState: () => ({
+    tools: {
+      job1: createdJob,
+    },
+  }),
+};
 
-test('toolsMiddleware', () => {
-  const action = { type: 'REHYDRATE_JOBS', payload: { your: 'data' } };
-  toolsMiddleware(store)(next)(action);
+describe('toolsMiddleware', () => {
+  it('should run with new jobs', () => {
+    const action = {
+      type: REHYDRATE_JOBS,
+    };
+    toolsMiddleware(store)(next)(action);
+    // expect(store.dispatch).toHaveBeenCalled();
+  });
 });
