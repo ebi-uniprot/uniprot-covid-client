4f5f6237c0decc9cf01a6680391886c6b53e8592
Daniel Rice <dlrice@ebi.ac.uk>
2020-06-09 16:56:43 +0100

    Test getJobMessage including snapshot.

--- src/tools/utils/__tests__/index.spec.tsx
+++ src/tools/utils/__tests__/index.spec.tsx
@@ -1,4 +1,7 @@
-import { getServerErrorDescription } from '..';
+import { getServerErrorDescription, getJobMessage } from '..';
+import createdJob from '../../blast/__mocks__/internal-jobs/created';
+import runningJob from '../../blast/__mocks__/internal-jobs/running';
+import { render } from '@testing-library/react';
 
 describe('getServerErrorDescription', () => {
   it('should get formatted error string from server error', () => {
@@ -10,5 +13,40 @@ describe('getServerErrorDescription', () => {
     const error = { response: { data } };
     expect(getServerErrorDescription(error)).toEqual(`Invalid parameters: 
         Sequence → Error in reading input sequence. Please check your input.`);
+  });
+});
+
+describe('getJobMessage', () => {
+  it('should create an error message object', () => {
+    expect(
+      getJobMessage({
+        job: createdJob,
+        errorDescription: 'Some error',
+      })
+    ).toEqual({
+      id: 'local-97e5ab00-9ff0-11ea-baf5-bf14c0760612',
+      format: 'POP_UP',
+      tag: 'JOB',
+      omitAndDeleteAtLocations: ['Dashboard'],
+      content: 'Some error',
+      level: 'failure',
+    });
+  });
+
+  it('should create a success message object', () => {
+    const jobMessage = getJobMessage({
+      job: runningJob,
+      nHits: 100,
+    });
+    const { asFragment } = render(jobMessage.content as JSX.Element);
+    expect(asFragment()).toMatchSnapshot();
+    delete jobMessage.content;
+    expect(jobMessage).toEqual({
+      id: 'local-97e5ab00-9ff0-11ea-baf5-bf14c9060612',
+      format: 'POP_UP',
+      tag: 'JOB',
+      omitAndDeleteAtLocations: ['Dashboard'],
+      level: 'success',
+    });
   });
 });
