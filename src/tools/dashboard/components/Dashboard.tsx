a771bfc4dcc20fab3c3c31793088e15ef1dbd36c
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2020-06-17 14:10:01 +0100

    added illustration for no-blast results. Updated test cases since Franklin components has been changed

--- src/tools/dashboard/components/Dashboard.tsx
+++ src/tools/dashboard/components/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Card, PageIntro, ClockIcon } from 'franklin-sites';
+import { Link } from 'react-router-dom';
+import { Card, PageIntro, ClockIcon, Message } from 'franklin-sites';
 
 import { Job } from '../../blast/types/blastJob';
 import { ToolsState } from '../../state/toolsInitialState';
@@ -8,6 +9,8 @@ import { ToolsState } from '../../state/toolsInitialState';
 import Row from './Row';
 
 import './styles/Dashboard.scss';
+import '../../../shared/components/error-pages/styles/error-pages.scss';
+import ArtWork from '../../svg/no-blast-results.svg';
 
 // temporary
 // import created from '../../blast/__mocks__/internal-jobs/created';
@@ -30,30 +33,48 @@ const Dashboard = () => {
   //   running,
   // ];
 
+  const noResultsSubtitle = (
+    <div>
+      Try using <Link to="/blast">BLAST</Link>, Align, ID Mapping/Retrieve or
+      Peptide Search to begin
+    </div>
+  );
+
   return (
     <>
       <PageIntro title="Tool results" />
-      <p>
-        Your tool analysis results from the last{' '}
-        <ClockIcon height="1em" width="3ch" /> 7 days are listed below. For any
-        tools still running, you can navigate away to other pages and will be
-        notified once the job is finished.
-      </p>
-      <div className="dashboard">
-        <div className="dashboard__header">
-          <Card>
-            <span>Name</span>
-            <span>Job type</span>
-            <span>Created</span>
-            <span>Status</span>
-          </Card>
+      {jobs && jobs.length > 0 ? (
+        <>
+          <p>
+            Your tool analysis results from the last{' '}
+            <ClockIcon height="1em" width="3ch" /> 7 days are listed below. For
+            any tools still running, you can navigate away to other pages and
+            will be notified once the job is finished.
+          </p>
+          <div className="dashboard">
+            <div className="dashboard__header">
+              <Card>
+                <span>Name</span>
+                <span>Job type</span>
+                <span>Created</span>
+                <span>Status</span>
+              </Card>
+            </div>
+            <div className="dashboard__body">
+              {jobs.map((job) => (
+                <Row job={job} key={job.internalID} />
+              ))}
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="error-page-container">
+          <ArtWork className="error-page-container__art-work" />
+          <Message level="warning" subtitle={noResultsSubtitle} forFullPage>
+            No results available. Your UniProt tool results will be shown here
+          </Message>
         </div>
-        <div className="dashboard__body">
-          {jobs.map((job) => (
-            <Row job={job} key={job.internalID} />
-          ))}
-        </div>
-      </div>
+      )}
     </>
   );
 };
