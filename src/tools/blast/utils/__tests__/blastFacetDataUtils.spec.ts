f470f06b212299de22038fac6d1a71e81d1348ee
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2020-07-02 23:22:43 +0100

    more test for blast filter params

--- src/tools/blast/utils/__tests__/blastFacetDataUtils.spec.ts
+++ src/tools/blast/utils/__tests__/blastFacetDataUtils.spec.ts
@@ -1,4 +1,8 @@
-import { filterBlastHitForFacets } from '../blastFacetDataUtils';
+import {
+  filterBlastHitForFacets,
+  getFacetParametersFromBlastHits,
+} from '../blastFacetDataUtils';
+import { SelectedFacet } from '../../types/blastResults';
 
 describe('filterBlastHitForFacets', () => {
   const datapoint = {
@@ -63,6 +67,118 @@ describe('filterBlastHitForFacets', () => {
       filterBlastHitForFacets(datapoint, rangeFilters, activeFacet)
     ).toEqual({
       identity: 10,
+    });
+  });
+});
+
+describe('getFacetParametersFromBlastHits', () => {
+  const selectedFacetsEmpty = [];
+
+  const selectedFacetsOne: SelectedFacet[] = [
+    {
+      name: 'score',
+      value: '0-99',
+    },
+  ];
+
+  const selectedFacetsTwo: SelectedFacet[] = [
+    {
+      name: 'score',
+      value: '0-101',
+    },
+    {
+      name: 'identity',
+      value: '0-12',
+    },
+  ];
+
+  const blastHits = [
+    {
+      hit_hsps: [
+        { hsp_score: 100, hsp_identity: 10, hsp_expect: 0.1 },
+        { hsp_score: 101, hsp_identity: 11, hsp_expect: 0.2 },
+        { hsp_score: 102, hsp_identity: 12, hsp_expect: 0.3 },
+      ],
+    },
+  ];
+
+  const blastHitsAllValues = {
+    score: {
+      values: [100, 101, 102],
+      min: 100,
+      max: 102,
+    },
+    identity: {
+      values: [10, 11, 12],
+      min: 10,
+      max: 12,
+    },
+    evalue: {
+      values: [0.1, 0.2, 0.3],
+      min: 0.1,
+      max: 0.3,
+    },
+  };
+
+  it('should return all if no facets have been set', () => {
+    const results = getFacetParametersFromBlastHits(
+      selectedFacetsEmpty,
+      '',
+      blastHits
+    );
+
+    expect(results).toEqual(blastHitsAllValues);
+  });
+
+  it('should return all values for active facet, regardless of facet value', () => {
+    const results = getFacetParametersFromBlastHits(
+      selectedFacetsOne,
+      'score',
+      blastHits
+    );
+
+    expect(results).toEqual({
+      score: {
+        values: [100, 101, 102],
+        min: 100,
+        max: 102,
+      },
+      identity: {
+        values: [],
+        min: 10,
+        max: 12,
+      },
+      evalue: {
+        values: [],
+        min: 0.1,
+        max: 0.3,
+      },
+    });
+  });
+
+  it('should return only in-range values, when facet is not active', () => {
+    const results = getFacetParametersFromBlastHits(
+      selectedFacetsTwo,
+      'identity',
+      blastHits
+    );
+
+    expect(results).toEqual({
+      score: {
+        values: [100, 101],
+        min: 100,
+        max: 102,
+      },
+      identity: {
+        values: [10, 11],
+        min: 10,
+        max: 12,
+      },
+      evalue: {
+        values: [0.1, 0.2],
+        min: 0.1,
+        max: 0.3,
+      },
     });
   });
 });
