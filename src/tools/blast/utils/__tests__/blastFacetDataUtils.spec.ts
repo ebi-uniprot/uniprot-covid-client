73e4ca12e00e99131c4fcdba1f8518c218fda546
Daniel Rice <dlrice@ebi.ac.uk>
2020-07-03 10:28:07 +0100

    Add filterBlastDataForResults test.

--- src/tools/blast/utils/__tests__/blastFacetDataUtils.spec.ts
+++ src/tools/blast/utils/__tests__/blastFacetDataUtils.spec.ts
@@ -2,9 +2,16 @@ import {
   filterBlastHitForFacets,
   ParsedBlastFacets,
   getFacetParametersFromBlastHits,
+  filterBlastDataForResults,
 } from '../blastFacetDataUtils';
 import { SelectedFacet } from '../../../../uniprotkb/types/resultsTypes';
 import { BlastFacet } from '../../types/blastResults';
+
+const blastHits = [
+  { hit_hsps: [{ hsp_score: 100, hsp_identity: 10, hsp_expect: 0.1 }] },
+  { hit_hsps: [{ hsp_score: 101, hsp_identity: 11, hsp_expect: 0.2 }] },
+  { hit_hsps: [{ hsp_score: 102, hsp_identity: 12, hsp_expect: 0.3 }] },
+];
 
 describe('filterBlastHitForFacets', () => {
   const datapoint = {
@@ -94,16 +101,6 @@ describe('getFacetParametersFromBlastHits', () => {
     },
   ];
 
-  const blastHits = [
-    {
-      hit_hsps: [
-        { hsp_score: 100, hsp_identity: 10, hsp_expect: 0.1 },
-        { hsp_score: 101, hsp_identity: 11, hsp_expect: 0.2 },
-        { hsp_score: 102, hsp_identity: 12, hsp_expect: 0.3 },
-      ],
-    },
-  ];
-
   const blastHitsAllValues = {
     score: {
       values: [100, 101, 102],
@@ -181,6 +178,40 @@ describe('getFacetParametersFromBlastHits', () => {
         min: 0.1,
         max: 0.3,
       },
+    });
+  });
+});
+
+describe('filterBlastDataForResults', () => {
+  it('should', () => {
+    const facet = [
+      {
+        name: 'score',
+        value: '100-101',
+      },
+    ];
+    expect(filterBlastDataForResults({ hits: blastHits }, facet)).toEqual({
+      hits: [
+        {
+          hit_hsps: [
+            {
+              hsp_score: 100,
+              hsp_identity: 10,
+              hsp_expect: 0.1,
+            },
+          ],
+        },
+        {
+          hit_hsps: [
+            {
+              hsp_score: 101,
+              hsp_identity: 11,
+              hsp_expect: 0.2,
+            },
+          ],
+        },
+      ],
+      alignments: 2,
     });
   });
 });
