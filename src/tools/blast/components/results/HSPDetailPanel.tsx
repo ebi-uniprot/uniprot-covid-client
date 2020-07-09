88a933ab4e03649fd2ebfa02d1823206c75329ef
Xavier Watkins <xavier.watkins@gmail.com>
2020-07-09 17:09:28 +0100

    Add comments

--- src/tools/blast/components/results/HSPDetailPanel.tsx
+++ src/tools/blast/components/results/HSPDetailPanel.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+/* eslint-disable @typescript-eslint/camelcase */
+import React, { FC, useCallback } from 'react';
 import ProtvistaTrack from 'protvista-track';
 import ProtvistaNavigation from 'protvista-navigation';
 import ProtvistaMSA from 'protvista-msa';
@@ -13,16 +14,103 @@ loadWebComponent('protvista-msa', ProtvistaMSA);
 loadWebComponent('protvista-manager', ProtvistaManager);
 
 const HSPDetailPanel: FC<{ hsp: BlastHsp }> = ({ hsp }) => {
-  console.log(hsp);
+  const {
+    hsp_align_len,
+    hsp_query_from,
+    hsp_query_to,
+    hsp_qseq,
+    hsp_hit_from,
+    hsp_hit_to,
+    hsp_hseq,
+  } = hsp;
+
+  // TODO calculate actual length based on total match and query lengths
+  const actualLength = hsp_align_len;
+
+  const setQueryTrackData = useCallback(
+    (node): void => {
+      if (node) {
+        // TODO calculate actual start|end based on actualLength
+        // eslint-disable-next-line no-param-reassign
+        node.data = [
+          {
+            start: hsp_query_from,
+            end: hsp_query_to,
+          },
+        ];
+      }
+    },
+    [hsp_query_from, hsp_query_to]
+  );
+
+  const setMatchTrackData = useCallback(
+    (node): void => {
+      if (node) {
+        // TODO calculate actual start|end based on actualLength
+        // eslint-disable-next-line no-param-reassign
+        node.data = [
+          {
+            start: hsp_hit_from,
+            end: hsp_hit_to,
+          },
+        ];
+      }
+    },
+    [hsp_hit_from, hsp_hit_to]
+  );
+
+  const setMSAData = useCallback(
+    (node): void => {
+      if (node) {
+        // eslint-disable-next-line no-param-reassign
+        node.data = [
+          {
+            name: 'Query',
+            sequence: hsp_qseq,
+          },
+          {
+            name: 'Match',
+            sequence: hsp_hseq,
+          },
+        ];
+      }
+    },
+    [hsp_qseq, hsp_hseq]
+  );
+
   return (
     <SlidingPanel position="bottom">
       <>
-        <h2>{hsp.hsp_expect}</h2>
-        <protvista-manager>
-          <protvista-navigation />
-          <protvista-msa />
-          <protvista-track />
-        </protvista-manager>
+        {/* TODO close link */}
+        <h4>Protein name - Organism</h4>
+        {/* TODO row of buttons - Highlight properties - Show annotation - View toggle */}
+        <section className="hsp-row">
+          <protvista-manager attributes="displaystart displayend">
+            <protvista-navigation length={hsp_align_len} />
+            <protvista-msa ref={setMSAData} length={hsp_align_len} />
+          </protvista-manager>
+        </section>
+        <section className="hsp-row">
+          {/* 
+          TODO listen to "highlight" event from block above and set on these 2 tracks,
+          working as protvista-manager
+          */}
+          {/* Query track */}
+          <protvista-track
+            height="10"
+            ref={setQueryTrackData}
+            length={actualLength}
+            layout="non-overlapping"
+          />
+          {/* Match track - to colour based on score, see BlastSummaryTrack in BlastResultTable */}
+          <protvista-track
+            height="10"
+            ref={setMatchTrackData}
+            length={actualLength}
+            layout="non-overlapping"
+          />
+          {/* TODO add configured feature tracks here */}
+        </section>
       </>
     </SlidingPanel>
   );
