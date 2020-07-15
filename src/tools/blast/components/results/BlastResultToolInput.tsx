b8bba3ccaa3770d0b4ce8d939a2a81c5f22ad03f
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-07-15 12:45:03 +0200

    add curl command line example

--- src/tools/blast/components/results/BlastResultToolInput.tsx
+++ src/tools/blast/components/results/BlastResultToolInput.tsx
@@ -4,14 +4,33 @@ import { Loader, CodeBlock, InfoList } from 'franklin-sites';
 import ErrorHandler from '../../../../shared/components/error-pages/ErrorHandler';
 
 import { UseDataAPIState } from '../../../../shared/hooks/useDataApi';
-import { PublicServerParameters } from '../../types/blastServerParameters';
+
+import toolsURLs from '../../../config/urls';
+
+import {
+  ServerParameters,
+  PublicServerParameters,
+} from '../../types/blastServerParameters';
+import { JobTypes } from '../../../types/toolsJobTypes';
+
+const inputToCurl = (input: Partial<ServerParameters>, jobType: JobTypes) => {
+  return `curl ${Object.entries(input)
+    .map(
+      ([key, value]) =>
+        `-F ${
+          typeof value === 'string' && value.includes("'") ? '$' : ''
+        }'${key}=${value}'`
+    )
+    .join(' \\\n')} \\\n${toolsURLs(jobType).runUrl}`;
+};
 
 type Props = {
   id: string;
   inputParamsData: Partial<UseDataAPIState<PublicServerParameters>>;
+  jobType: JobTypes;
 };
 
-const BlastResultToolInput: FC<Props> = ({ id, inputParamsData }) => {
+const BlastResultToolInput: FC<Props> = ({ id, inputParamsData, jobType }) => {
   const { loading, data, error, status } = inputParamsData;
 
   if (loading) return <Loader />;
@@ -36,6 +55,22 @@ const BlastResultToolInput: FC<Props> = ({ id, inputParamsData }) => {
         <a href="https://www.ebi.ac.uk/seqdb/confluence/pages/viewpage.action?pageId=94147939#NCBIBLAST+HelpandDocumentation-RESTAPI">
           API documentation page
         </a>
+      </p>
+      <p>
+        Using{' '}
+        <a
+          href="https://curl.haxx.se/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          curl
+        </a>
+        , you could run a new job on the command line with the same input like
+        this:
+        <br />
+        <CodeBlock lightMode>
+          {inputToCurl({ email: '<enter your email here>', ...data }, jobType)}
+        </CodeBlock>
       </p>
     </section>
   );
