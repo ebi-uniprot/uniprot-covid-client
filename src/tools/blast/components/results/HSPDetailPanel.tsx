2fb9d52cd27f7e9b2e7e0795c4c3b2519e84ca1f
Daniel Rice <dlrice@ebi.ac.uk>
2020-07-18 12:14:58 +0100

    Create menu. Select highlight.

--- src/tools/blast/components/results/HSPDetailPanel.tsx
+++ src/tools/blast/components/results/HSPDetailPanel.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable @typescript-eslint/camelcase */
-import React, { FC, useCallback, useEffect, useState, useRef } from 'react';
+import React, {
+  FC,
+  useCallback,
+  useEffect,
+  useState,
+  useRef,
+  Fragment,
+} from 'react';
 import ProtvistaTrack from 'protvista-track';
 import ProtvistaNavigation from 'protvista-navigation';
 import ProtvistaMSA from 'protvista-msa';
 import ProtvistaManager from 'protvista-manager';
-import { Loader } from 'franklin-sites';
+import { Loader, DropdownButton, DownloadIcon } from 'franklin-sites';
+import { uniqBy, uniq } from 'lodash-es';
 import SlidingPanel from '../../../../shared/components/layouts/SlidingPanel';
 import { BlastHsp } from '../../types/blastResults';
 import { loadWebComponent } from '../../../../shared/utils/utils';
@@ -14,6 +22,8 @@ import { UniProtkbAPIModel } from '../../../../uniprotkb/adapters/uniProtkbConve
 import apiUrls from '../../../../uniprotkb/config/apiUrls';
 import FeatureType from '../../../../uniprotkb/types/featureType';
 import { processFeaturesData } from '../../../../uniprotkb/components/protein-data-views/FeaturesView';
+import { fileFormatEntryDownload } from '../../../../uniprotkb/types/resultsTypes';
+import AddToBasketButton from '../../../../shared/components/action-buttons/AddToBasket';
 
 loadWebComponent('protvista-navigation', ProtvistaNavigation);
 loadWebComponent('protvista-track', ProtvistaTrack);
@@ -35,11 +45,11 @@ const HSPDetailPanel: FC<HSPDetailPanelProps> = ({ hsp, hitAccession }) => {
     hsp_hit_to,
     hsp_hseq,
   } = hsp;
-  console.log('hsp:', hsp, 'accession', hitAccession);
+  // console.log('hsp:', hsp)
   // TODO calculate actual length based on total match and query lengths
   const actualLength = hsp_align_len;
   const [highlightPosition, setHighlighPosition] = useState('');
-  const [activeTrack, setActiveTrack] = useState('');
+  const [annotation, setAnnotation] = useState<FeatureType>();
   // const featureTrackRef = useRef();
 
   const regex = /([-]+)/gm;
@@ -110,20 +120,27 @@ const HSPDetailPanel: FC<HSPDetailPanelProps> = ({ hsp, hitAccession }) => {
     },
     [hsp_qseq, hsp_hseq]
   );
-  const { loading, data, error } = useDataApi<UniProtkbAPIModel>(
+  // TODO deal with error if present
+  const { loading, data } = useDataApi<UniProtkbAPIModel>(
     apiUrls.entry(hitAccession)
   );
   const features = data?.features;
+
+  const annotationChoices = uniq(features?.map(({ type }) => type));
+  if (!annotation && !!annotationChoices?.length) {
+    setAnnotation(annotationChoices[0]);
+  }
+
   const setFeatureTrackData = useCallback(
     (node): void => {
-      if (node && features) {
+      if (node && features && annotation) {
         const processedFeatures = processFeaturesData(
-          features.filter(({ type }) => type === FeatureType.DOMAIN)
+          features.filter(({ type }) => type === annotation)
         );
         node.data = processedFeatures;
       }
     },
-    [features]
+    [features, annotation]
   );
 
   const navRef = useCallback((node): void => {
@@ -151,7 +168,49 @@ const HSPDetailPanel: FC<HSPDetailPanelProps> = ({ hsp, hitAccession }) => {
       <>
         {/* TODO close link */}
         <h4>Protein name - Organism</h4>
-        {/* TODO row of buttons - Highlight properties - Show annotation - View toggle */}
+        <div className="button-group">
+          <DropdownButton label="Highlight properties" className="tertiary">
+            <div className="dropdown-menu__content">
+              <ul>
+                {fileFormatEntryDownload.map((fileFormat) => (
+                  <li key={fileFormat}>{fileFormat}</li>
+                ))}
+              </ul>
+            </div>
+          </DropdownButton>
+          {!!annotationChoices?.length && (
+            <DropdownButton label="Show annotation" className="tertiary">
+              {(setShowMenu: (showMenu: boolean) => void) => (
+                <div className="dropdown-menu__content">
+                  <ul>
+                    {annotationChoices.map((annotationChoice) => (
+                      // TODO: set currently selected
+                      <li key={annotation}>
+                        <button
+                          type="button"
+                          onClick={() => {
+                            setShowMenu(false);
+                            setAnnotation(annotationChoice);
+                          }}
+                        >
+                          {annotation}
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </DropdownButton>
+          )}
+          <DropdownButton label="View" className="tertiary">
+            <div className="dropdown-menu__content">
+              <ul>
+                <li key="overview">Overview</li>
+                <li key="wrapped">Wrapped</li>
+              </ul>
+            </div>
+          </DropdownButton>
+        </div>
         <section className="hsp-row">
           <protvista-manager attributes="displaystart displayend">
             <protvista-navigation ref={navRef} length={hsp_align_len} />
