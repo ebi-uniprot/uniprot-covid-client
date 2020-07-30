5401542ea64c6b7d3467bf50967a63e2f7a5e7df
Aurélien Luciani <aurel-l@users.noreply.github.com>
2020-07-30 17:03:36 +0200

    fix handling of no result

--- src/uniprotkb/components/results/ResultsContainer.tsx
+++ src/uniprotkb/components/results/ResultsContainer.tsx
@@ -74,7 +74,8 @@ const Results: FC = () => {
 
   const total = headers?.['x-totalrecords'];
 
-  if (total === 0) {
+  // no results if total is 0, or if not loading anymore and still no total info
+  if (total === 0 || !(total || loading)) {
     return <NoResultsPage />;
   }
 
