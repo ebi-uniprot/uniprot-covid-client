21eafdeedd8937ac3dd05368e9b5f790d0932eba
Daniel Rice <dlrice@ebi.ac.uk>
2021-10-07 11:49:45 +0100

    Remove sentry.

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,66 +1,67 @@
 import React, { lazy, Suspense } from 'react';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { FranklinSite, Loader } from 'franklin-sites';
-import * as Sentry from '@sentry/browser';
 import BaseLayout from '../../shared/components/layouts/BaseLayout';
 import { Location, LocationToPath } from '../config/urls';
 import './styles/app.scss';
 import history from '../../shared/utils/browserHistory';
 
-if (process.env.NODE_ENV !== 'development') {
-  Sentry.init({
-    dsn: 'https://be99e24b352b42019d5b9f53dd7b68c3@sentry.io/1770286',
-  });
-}
-
 // Async loading of page components
 // const HomePage = lazy(() =>
 //   import(/* webpackChunkName: "home-page" */ './HomePage')
 // );
-const ResultsPage = lazy(() =>
-  import(
-    /* webpackChunkName: "results" */ '../../uniprotkb/components/results/ResultsContainer'
-  )
+const ResultsPage = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "results" */ '../../uniprotkb/components/results/ResultsContainer'
+    )
 );
-const EntryPage = lazy(() =>
-  import(
-    /* webpackChunkName: "entry" */ '../../uniprotkb/components/entry/Entry'
-  )
+const EntryPage = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "entry" */ '../../uniprotkb/components/entry/Entry'
+    )
 );
 // const AdvancedSearchPage = lazy(() =>
 //   import(
 //     /* webpackChunkName: "advanced-search" */ '../../uniprotkb/components/query-builder/AdvancedSearchContainer'
 //   )
 // );
-const CustomiseTablePage = lazy(() =>
-  import(
-    /* webpackChunkName: "customise-table" */ '../../uniprotkb/components/customise-table/CustomiseTableContainer'
-  )
+const CustomiseTablePage = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "customise-table" */ '../../uniprotkb/components/customise-table/CustomiseTableContainer'
+    )
 );
-const DownloadPage = lazy(() =>
-  import(
-    /* webpackChunkName: "download" */ '../../uniprotkb/components/download/DownloadContainer'
-  )
+const DownloadPage = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "download" */ '../../uniprotkb/components/download/DownloadContainer'
+    )
 );
-const ResourceNotFoundPage = lazy(() =>
-  import(
-    /* webpackChunkName: "resource-not-found" */ '../../shared/components/error-pages/ResourceNotFoundPage'
-  )
+const ResourceNotFoundPage = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "resource-not-found" */ '../../shared/components/error-pages/ResourceNotFoundPage'
+    )
 );
-const ServiceUnavailablePage = lazy(() =>
-  import(
-    /* webpackChunkName: "service-unavailable" */ '../../shared/components/error-pages/ServiceUnavailablePage'
-  )
+const ServiceUnavailablePage = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "service-unavailable" */ '../../shared/components/error-pages/ServiceUnavailablePage'
+    )
 );
-const JobErrorPage = lazy(() =>
-  import(
-    /* webpackChunkName: "job-error" */ '../../shared/components/error-pages/JobErrorPage'
-  )
+const JobErrorPage = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "job-error" */ '../../shared/components/error-pages/JobErrorPage'
+    )
 );
-const ContactPage = lazy(() =>
-  import(
-    /* webpackChunkName: "contact" */ '../../shared/components/contact/ContactPage'
-  )
+const ContactPage = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "contact" */ '../../shared/components/contact/ContactPage'
+    )
 );
 
 const App = () => (
